f7c52e02f4b193c0b0d1a200f07c220516b9571d
Porcher  Jeanne <jep4000@robotarium.cm.cluster>
2025-04-03 19:51:50 +0100

    Full runtimes

--- Godugu_Anil_MPI/benchmark_parallel.sh
+++ Godugu_Anil_MPI/benchmark_parallel.sh
@@ -1,17 +1,17 @@
-echo "input,cores,run1,run2,run3" > runtime_parallel.csv
+echo "input,cores,run1,run2,run3" > runtime_parallel1.csv
 
-for inputSize in 15000 30000 100000
+for inputSize in 30000 100000
 do
-    for cores in 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128 132 136 140 144 148 152 156 160 164 168 172 176 180 184 188 192
+    for cores in 192 188 184 180 176 172 168 164 160 156 152 148 144 140 136 132 128 124 120 116 112 108 104 100 96 92 88 84 80 76 72 68 64 60 56 52 48 44 40 36 32 28 24 20 16 12 8 4 2 1
     do
-        echo -n $inputSize >> runtime_parallel.csv
-        echo -n "," >> runtime_parallel.csv
-        echo -n $cores >> runtime_parallel.csv
+        echo -n $inputSize >> runtime_parallel1.csv
+        echo -n "," >> runtime_parallel1.csv
+        echo -n $cores >> runtime_parallel1.csv
         for k in 1 2 3
         do
-            echo -n "," >> runtime_parallel.csv
-            printf "%s" "$(srun --partition=amd-longq --mpi=pmi2 -N 3 -n $cores ./euler_totient_mpi $inputSize)" >> runtime_parallel.csv
+            echo -n "," >> runtime_parallel1.csv
+            printf "%s" "$(srun --partition=amd-longq --mpi=pmi2 -N 3 -n $cores ./euler_totient_mpi $inputSize)" >> runtime_parallel1.csv
         done
-        echo "" >> runtime_parallel.csv
+        echo "" >> runtime_parallel1.csv
     done
 done
--- Godugu_Anil_MPI/euler_totient2.c
+++ Godugu_Anil_MPI/euler_totient2.c
@@ -22,15 +22,15 @@ long int euler_len(long int n) {
 }
 
 int main(int argc, char** argv) {
-    int p, id;
+    int rank, size;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &p);
-    MPI_Comm_size(MPI_COMM_WORLD, &id);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (argc < 2) {
-        if (id == 0) {
-            printf("Error no params passed to euler_range\n");
+        if (rank == 0) {
+            printf("Error: No parameter passed to euler_range\n");
         }
         MPI_Finalize();
         return 1;
@@ -39,30 +39,28 @@ int main(int argc, char** argv) {
     int euler_range = atoi(argv[1]);
     long int total_totient = 0;
 
-    // If main process
-    if (p == 0) {
+    if (rank == 0) {
+        // Processus maître
         double start_time = MPI_Wtime();
         int next_task = 1;
-        int num_workers = id - 1;
+        int num_workers = size - 1;
         MPI_Status status;
 
-        // Assigning tasks for each other available process
+        // Envoi initial des tâches aux travailleurs
         for (int i = 1; i <= num_workers; i++) {
             if (next_task <= euler_range) {
                 MPI_Send(&next_task, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
                 next_task++;
-            } else {  // Terminating if the next_task is close to euler_range
+            } else {
                 int term = 0;
                 MPI_Send(&term, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
             }
         }
 
-        // Dynamic distribution for longer euler_ranges
+        // Gestion dynamique des tâches
         while (next_task <= euler_range) {
             long int result;
             MPI_Recv(&result, 1, MPI_LONG, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-
-            // Incrementing the totient based on the result
             total_totient += result;
 
             int task = next_task;
@@ -70,12 +68,10 @@ int main(int argc, char** argv) {
             next_task++;
         }
 
-        // Terminating the other processes
+        // Récupération des derniers résultats et envoi du signal d'arrêt
         for (int i = 1; i <= num_workers; i++) {
             long int result;
             MPI_Recv(&result, 1, MPI_LONG, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-
-            // Incrementing the totient for the final results
             total_totient += result;
 
             int term = 0;
@@ -83,26 +79,19 @@ int main(int argc, char** argv) {
         }
 
         double end_time = MPI_Wtime();
-        printf("%ld -> %.2fs\n", total_totient, end_time - start_time);
-    }
-    
-    else {
+        printf("Totient sum: %ld -> Execution time: %.2fs\n", total_totient, end_time - start_time);
+    } else {
+        // Processus esclaves
         while (1) {
             int task;
             MPI_Status status;
-
-            // Retrieving euler_range for each task
             MPI_Recv(&task, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-            // Exit condition
             if (status.MPI_TAG == 1) {
-                break;
+                break; // Arrêt du travailleur
             }
 
-            // Actual calculation process
             long int totient = euler_len(task);
-
-            // Sharing the result
             MPI_Send(&totient, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
         }
     }
